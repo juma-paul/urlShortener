c7f7f39aea7f4c3f6b42626d25c1ae3cbb5a84eb
juma <raisben7@gmail.com>
2024-07-24 16:54:58 -0700

    Increase max length of input url to 2048

--- shortener/models.py
+++ shortener/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class URL(models.Model):
-    long_url = models.URLField(null=True, blank=True)
+    long_url = models.URLField(max_length=2048, null=True, blank=True)
     short_url = models.CharField(max_length=7, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
