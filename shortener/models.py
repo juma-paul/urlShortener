e9b0f14aba1ae80385f385bcdfeaa633a76e6de9
juma <raisben7@gmail.com>
2024-07-23 20:31:48 -0700

    Create model for the url shortener

--- shortener/models.py
+++ shortener/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class URL(models.Model):
+    long_url = models.URLField(null=True, blank=True)
+    short_url = models.CharField(max_length=7, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.short_url} -> {self.long_url}"
