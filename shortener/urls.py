da95d0c0ff75ade25145197546c15362de3f2100
juma <raisben7@gmail.com>
2024-07-24 19:24:08 -0700

    Add a homepage url and a new url to point to index

--- shortener/urls.py
+++ shortener/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.homepage, name='homepage'),
+    path('shorten/', views.index, name='index'),
     path('<str:short_code>/', views.redirect_url, name='redirect_url'),
 ]
